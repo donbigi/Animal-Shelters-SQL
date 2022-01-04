8134f29231a3010bc070da5308c8cb401240bbe1
donbigi <ucosibe@gmail.com>
2022-01-04 19:17:10 +0100

    updated solution

--- Animal_shelters_SQL_solution.sql
+++ Animal_shelters_SQL_solution.sql
@@ -1,4 +1,54 @@
 -- Analysis of costs to run animal shelters
+-- Uploading csv files to postgreeSQL
+
+DROP TABLE IF EXISTS "animals";
+DROP TABLE IF EXISTS "age_costs";
+DROP TABLE IF EXISTS "location_costs";
+DROP TABLE IF EXISTS "sponsored_animals";
+DROP TABLE IF EXISTS "size_costs";
+
+-- Creating required tables
+
+CREATE TABLE animals (
+animalid VARCHAR(50) PRIMARY KEY,
+birthdate VARCHAR(50),
+animaltype VARCHAR(50),
+color VARCHAR(50),
+weight REAL,
+location VARCHAR(50)
+);
+
+CREATE TABLE age_costs (
+age VARCHAR(50) PRIMARY KEY,
+costs INT
+);
+
+CREATE TABLE location_costs (
+location VARCHAR(50) PRIMARY KEY,
+costs INT
+);
+
+CREATE TABLE sponsored_animals (
+sponsorid varchar(50) PRIMARY KEY,
+animaltype VARCHAR(50),
+location VARCHAR(50)
+);
+
+CREATE TABLE size_costs (
+sizeid VARCHAR(50) PRIMARY KEY,
+animaltype VARCHAR(50),
+size VARCHAR(50),
+costs INT
+);
+
+--Copying CSV file to PostgreeSQL
+copy animals FROM '/animal_data.csv' DELIMITER ',' CSV HEADER;
+copy sponsored_animals FROM 'sponsored_pets.csv' DELIMITER ',' CSV HEADER;
+copy age_costs FROM 'age_costs.csv' DELIMITER ',' CSV HEADER;
+copy location_costs FROM 'location_costs.csv' DELIMITER ',' CSV HEADER;
+copy size_costs FROM 'size_costs.csv' DELIMITER ',' CSV HEADER;
+
+
 WITH no_sponsored AS -- Creating a CTE table to remove all sponsored animal from selection
 	(
 	SELECT animalid,
